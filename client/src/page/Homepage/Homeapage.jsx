ac75d63513df86f168c0143b9b7d1ac5c1fb2003
Mahammed-Siyabuddeen <mohd.sihab615@gmail.com>
2023-05-26 07:51:53 +0530

    headercomponents

--- client/src/page/Homepage/Homeapage.jsx
+++ client/src/page/Homepage/Homeapage.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import './homeapage.css'
 import Navbarcomponent from '../../Component/Navbarcomponent/Navbarcomponent'
 import EventComponent from '../../Component/EventComponent/EventComponent'
+import HeaderComponent from '../../Component/HeaderComponent/HeaderComponent'
 
 const Homeapage = () => {
     return (
         <div>
             <Navbarcomponent />
+            <HeaderComponent/>
             <EventComponent />
         </div>
     )
