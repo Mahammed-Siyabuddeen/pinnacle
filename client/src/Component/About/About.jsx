1f4151cc2bda23265fda8eca78e043cc60d0fd4d
Mahammed-Siyabuddeen <mohd.sihab615@gmail.com>
2023-05-28 11:13:01 +0530

    content

--- client/src/Component/About/About.jsx
+++ client/src/Component/About/About.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import './About.css'
-function About(){
+function About() {
     return (
-     <div className="about">
-        <h1>Pinnacle</h1>
-        <p>SYGMA 2023 is a National level IT Fest organized by the BCA Department of Shri Dharmasthala Manjunatheshwara College of Business Management, Mangaluru. The Institution focuses on enhancing the learning experience for the students and fostering career readiness. The Theme chosen for the ‘SYGMA 2023’ is: SYGMA NEO-SAMKHYA – ‘A Universe of Endless Possibilities’. Neo-Samkhya is the exploration of the nature of reality and consciousness within a virtual world. It deals with nature of reality, the self and the universe. Neo-Samkhya emphasis on the realistic relationship and could be seen as relevant to the virtual reality experiences of the Metaverse, where digital content and virtual environments are created and experienced by conscious beings. Neo-Samkhya, the theme of SYGMA 2023 provides a platform to the techies where they will encounter the challenges that test their worth for a title.</p>
-    
-     </div>
+        <div className="about">
+            <h1>Pinnacle</h1>
+            <p>We live in an era of change and progress. Technology has taken the modern dynamic world by storm. The change that sweeps across the socio-economic spectrum of our society is so impressive that what is in trend now becomes outdated after a little time.   Those who refuse to sail along the changed set-up are abandoned. The human race, the world over, appears to have made an unqualified revolution to go with and for change.
+                The impact of change and development is considerably high on the young generation. It is destined to accept the new environment and contribute to the continuance of the system. This requires adequate knowledge of the system and the rapidity of change. A first-hand feel of the paradigm is extensively called for. The work of Educational Institutions is clearly cut out in this regard. They have to be in a position to provide the students with whatever is necessary to enable them to forge ahead. The inborn potential of the students should be brought to the forefront, their creativity be appreciated and nurtured; their enthusiasm be supported. An occasional pat on the back will work miracles in student achievements.
+                To achieve great feats, one must have a vision and a positive frame of mind.</p>
+        </div>
     )
 }
 
